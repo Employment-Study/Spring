440d4e96b62016ce4263ef60ac6ca150eb1190d1
Sunro Lee <tjsfhdhkd@naver.com>
2023-11-19 00:24:18 +0900

    Spring 입문 스프링부트(JPA 활용까지)

--- Spring Boot/hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ Spring Boot/hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,12 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //identity란 DB에서 자동으로 생성해주는 값
     private Long id;
     private String name;
 
